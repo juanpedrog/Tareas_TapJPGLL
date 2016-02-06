e01efbe513f206ccf24a9172fc484f294e202f10
juanpedrog <juanpedrogilllanos@hotmail.com>
2016-02-06 09:19:09 -0600

    Primera version

---
+++ Ventana.java
@@ -0,0 +1,38 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Ventana{
+	public Ventana(){
+		construyeVentana();
+	}
+	public void construyeVentana(){
+		//Declarando e inicializando variables
+		JFrame ventana=new JFrame("Register Account");
+		JLabel user=new JLabel("User");
+		JLabel pass=new JLabel("Password");
+		JLabel comments=new JLabel("Comments");
+		JButton ok=new JButton("Ok");
+		JButton cancel=new JButton("Cancel");
+		JTextField usertext=new JTextField("");
+		JTextField passtext=new JTextField("");
+		JTextArea commentstext=new JTextArea("");
+
+		//Asignando formato a la ventana
+		ventana.setLayout(new GridLayout());
+		ventana.add(user);
+		ventana.add(pass);
+		ventana.add(comments);
+		ventana.add(ok);
+		ventana.add(cancel);
+		ventana.add(usertext);
+		ventana.add(passtext);
+		ventana.add(commentstext);
+		ventana.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		ventana.pack();
+		ventana.setVisible(true);
+	}
+
+	public static void main(String[] args){
+		new Ventana();
+	}
+}
